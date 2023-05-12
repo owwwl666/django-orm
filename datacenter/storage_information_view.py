1853572a6744b1d5062b9e3ac50614b1f3b16a9f
owwwl666 <zaharsalam228@gmail.com>
2023-05-12 14:05:20 +1000

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -5,14 +5,15 @@ from django.shortcuts import render
 def storage_information_view(request):
     """Отрисовываем список пользователей, находящихся в хранилище"""
     non_closed_visits = []
-    visits = Visit.objects.filter(leaved_at=None)
+    visits_for_now = Visit.objects.filter(leaved_at=None)
 
-    for visit in visits:
+    for visit in visits_for_now:
         non_closed_visits += [
             {
                 'who_entered': visit.passcard.owner_name,
                 'entered_at': visit.entered_at,
                 'duration': visit.format_duration,
+                'is_strange': visit.is_visit_long,
             }
         ]
 
